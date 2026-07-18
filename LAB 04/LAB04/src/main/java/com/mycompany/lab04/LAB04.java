042ed75171bf47f30c34ee712cacef451a43903a
rayniroshana_11 <nawarat-ps23191@stu.kln.ac.lk>
2026-07-18 23:07:15 +0530

    lab04 q2 lecturer child class

---
+++ LAB 04/LAB04/src/main/java/com/mycompany/lab04/LAB04.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.lab04;
+
+/**
+ *
+ * @author Raini
+ */
+public class LAB04 {
+
+   
+    public static void main(String[] args) {
+        Lecturer lecturer1 = new Lecturer("Nimal Perera", "L001", "Computer Science", 3, 15000.0);
+
+        lecturer1.displayLecturerDetails();
+        System.out.println("Monthly Payment: " + lecturer1.calculateMonthlyPayment());
+    }
+}
