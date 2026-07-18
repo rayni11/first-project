2cbed4ecf40416cd080501593a2507aff0f69618
rayniroshana_11 <nawarat-ps23191@stu.kln.ac.lk>
2026-07-18 23:14:29 +0530

    lab04 q3 second child class

--- LAB 04/LAB04/src/main/java/com/mycompany/lab04/LAB04.java
+++ LAB 04/LAB04/src/main/java/com/mycompany/lab04/LAB04.java
@@ -10,11 +10,17 @@ package com.mycompany.lab04;
  */
 public class LAB04 {
 
-   
+
     public static void main(String[] args) {
         Lecturer lecturer1 = new Lecturer("Nimal Perera", "L001", "Computer Science", 3, 15000.0);
+        LabAssistant assistant1 = new LabAssistant("Kamal Silva", "LA001", "Physics", 80, 500.0);
 
         lecturer1.displayLecturerDetails();
         System.out.println("Monthly Payment: " + lecturer1.calculateMonthlyPayment());
+
+        System.out.println();
+
+        assistant1.displayLabAssistantDetails();
+        System.out.println("Monthly Payment: " + assistant1.calculateMonthlyPayment());
     }
 }
---
+++ LAB 04/LAB04/src/main/java/com/mycompany/lab04/LabAssistant.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.lab04;
+
+/**
+ *
+ * @author Raini
+ */
+public class LabAssistant extends StaffMember {
+    private int hoursWorked;
+    private double hourlyRate;
+
+    public LabAssistant(String fullName, String staffId, String department,
+                         int hoursWorked, double hourlyRate) {
+        super(fullName, staffId, department);
+        this.hoursWorked = hoursWorked;
+        this.hourlyRate = hourlyRate;
+    }
+
+    @Override
+    public double calculateMonthlyPayment() {
+        return hoursWorked * hourlyRate;
+    }
+
+    public void displayLabAssistantDetails() {
+        displayBasicDetails();
+        System.out.println("Hours Worked: " + hoursWorked);
+        System.out.println("Hourly Rate: " + hourlyRate);
+    }
+
+    // department can be used here even though it's declared in StaffMember, because
+    // it is marked "protected" - protected members are visible to subclasses, not just
+    // to the class that declares them.
+}
+    
+
